5f029b64a88a914dbfda388f5c38ff6978b24d4f
Kerry <kerrychen0815@gmail.com>
2024-11-19 21:33:15 -0800

    create HW3

--- Homework/Make-Up/PlotResponse.m
+++ Homework/Make-Up/PlotResponse.m
@@ -14,6 +14,9 @@ figure(1)
 plot(z,'o');
 hold on
 plot(p,'*');
+title("Poles and Zeros");
+ylabel("j\omega");
+xlabel("\sigma");
 grid on
 
 T = tf(num, den);
@@ -23,6 +26,9 @@ figure('Position',[900 750 400 250]);
 figure(2);
 semilogx(w, 20*log10(abs(H)));
 grid on
+title("Magnitude response");
+ylabel("Megnitude [dB]");
+xlabel("Frequency [Hz]");
 xline(2*pi*20e3);
 
 % Phase response
@@ -30,7 +36,9 @@ figure('Position',[900 400 400 250]);
 figure(3);
 % semilogx(w, 180/pi*phase(H));
 semilogx(w, 180/pi*unwrap(angle(H)));
-
+title("Phase response");
+ylabel("Phase [degrees]");
+xlabel("Frequency [Hz]");
 grid on
 
 % Group delay
@@ -39,10 +47,14 @@ gd = -diff(unwrap(angle(H)))./diff(w);
 figure('Position',[900 50 400 250]);
 figure(4);
 semilogx(w(1:length(w)-1), gd);
+ylabel("Group Delay [s]");
+xlabel("Frequency [Hz]");
+title("Group delay");
 grid on
 
 % Step response
 figure('Position',[400 50 400 400]);
 figure(5)
 step(T)
+title("Step response");
 grid on
